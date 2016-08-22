138e492fdbe17efd89c88b08eb25832b68f04490
Winston <Winston@winstons-mbp.home>
2016-08-22 17:37:39 -0400

    finished step A

--- NetBeansProjects/FinalProject/src/finalproject/FinalProject.java
+++ NetBeansProjects/FinalProject/src/finalproject/FinalProject.java
@@ -1,14 +1,20 @@
 package finalproject;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class FinalProject {
 
-    public static void main(String[] args) {
-        if (0 < args.length) {
-            String filename = args[0];
-            File file = new File(filename);
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner read = new Scanner(new File("/Users/Winston/Downloads/worldcities.txt"));
+        ArrayList<String> list = new ArrayList<String>();
+        while (read.hasNextLine()) {
+            list.add(read.nextLine( ));
         }
+        read.close();
+        System.out.println(list);
     }
 }
+    
