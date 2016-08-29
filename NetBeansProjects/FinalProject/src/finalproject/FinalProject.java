235f2afbb0630668e82d5ff9db7c96577c8b5a5f
Winston <Winston@winstons-mbp.home>
2016-08-29 19:20:02 -0400

    changed to buffered reader

--- NetBeansProjects/FinalProject/src/finalproject/FinalProject.java
+++ NetBeansProjects/FinalProject/src/finalproject/FinalProject.java
@@ -2,19 +2,23 @@ package finalproject;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.util.Scanner;
 import java.util.ArrayList;
-
+import java.io.FileReader;
+import java.io.IOException;
 public class FinalProject {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner read = new Scanner(new File("/Users/Winston/Downloads/worldcities.txt"));
-        ArrayList<String> list = new ArrayList<String>();
-        while (read.hasNextLine()) {
-            list.add(read.nextLine( ));
-        }
-        read.close();
-        System.out.println(list);
+    public static void main(String[] args) throws IOException {
+      ArrayList<String> list = new ArrayList<String>();
+      FileReader input = new FileReader("/Users/Winston/Downloads/worldcities.txt");
+      BufferedReader read = new BufferedReader(input);
+      String buffer;
+      while ((buffer = read.readLine()) != null){
+          list.add(read.readLine());
+          list.add("\n");
+      }
+      System.out.println(list);
     }
 }
     
