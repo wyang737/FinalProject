7050f302e84b02fbd0e08ccf04e027d1fff24afa
Winston <Winston@winstons-mbp.home>
2016-08-31 12:48:34 -0400

    Finished City Search function

--- NetBeansProjects/FinalProject/src/finalproject/FinalProject.java
+++ NetBeansProjects/FinalProject/src/finalproject/FinalProject.java
@@ -12,19 +12,30 @@ public class FinalProject {
     public static void searchState(List<String> lst) {
         Scanner read = new Scanner(System.in);
         System.out.println("Enter a state to search for: ");
-        String input = read.next();
+        String input = read.nextLine();
+        System.out.println("");
+        System.out.println("Cities in " + input + " :");
         for (int i = 0; i < Integer.parseInt(lst.get(0)); i++) {
             int x = (i + 1) * 6 - 4;
             String buffer = lst.get(x).replace(input.trim(), "");
             String output = buffer.replace(",", "");
-            if (lst.get(x) != buffer){
-            System.out.println(output);
-            }
-           /* if (lst.get(x).toLowerCase().contains(input)) {
-                String buffer = lst.get(x).replace(input, "");
-                String output = buffer.replace(",", "");
+            if (lst.get(x) != buffer) {
                 System.out.println(output);
-            } */
+                System.out.println("ID number of " + output + ": " + i);
+            }
+        }
+    }
+
+    public static void searchCity(List<String> lst) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("Enter a city to search for: ");
+        String input = read.next();
+        for (int i = 0; i < Integer.parseInt(lst.get(0)); i++) {
+            int x = (i + 1) * 6 - 4;
+            if (lst.get(x).contains(input)) {
+                System.out.println("ID number of " + input + " :" + i);
+            }
         }
     }
 
@@ -38,6 +49,12 @@ public class FinalProject {
             list.add("\n");
         }
         System.out.println(list);
+        for (; ;){
+          System.out.println("Next Action: ");  
+        }
+            
+       
         searchState(list);
+        searchCity(list);
     }
 }
