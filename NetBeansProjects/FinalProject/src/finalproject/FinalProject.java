14524b03c2590faa911efcba15fc0194667fd82d
Winston <Winston@winstons-mbp.home>
2016-08-31 23:15:24 -0400

    Finished distances function

--- NetBeansProjects/FinalProject/src/finalproject/FinalProject.java
+++ NetBeansProjects/FinalProject/src/finalproject/FinalProject.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 
 public class FinalProject {
 
@@ -39,17 +40,59 @@ public class FinalProject {
         }
     }
 
-    public static void setCity(List<String> lst) {
+    public static void findDistance(List<String> lst) {
         Scanner read = new Scanner(System.in);
         System.out.println("");
         System.out.println("Enter an ID (0-10) to remember: ");
         String input = read.next();
         int ID = Integer.parseInt(input);
-        for (int i = 0; i < Integer.parseInt(lst.get(0)); i++){
-            if (i == ID){
+        for (int i = 0; i < Integer.parseInt(lst.get(0)); i++) {
+            if (i == ID) {
                 System.out.println("Currently selected city: " + lst.get((i + 1) * 6 - 4));
             }
         }
+        Scanner read1 = new Scanner(System.in);
+        System.out.println("");
+        System.out.println("Enter number of closest cities desired: 1-10");
+        String input1 = read.next();
+        List<String> buffer = new ArrayList<>(lst);
+        for (int i = 62; i > 51; i--) {
+            buffer.remove(62 - (62 - i) * 6);
+        }
+        buffer.removeAll(Collections.singleton("\n"));
+        buffer.remove(0);
+        List<Double> coordinates = new ArrayList<>();
+        for (String s : buffer) {
+            coordinates.add(Double.valueOf(s));
+        }
+        Double lat1 = coordinates.get(ID * 4 - ID * 2);
+        Double long1 = coordinates.get((ID * 4 - ID * 2) + 1);
+        List<Double> distances = new ArrayList<>();
+        for (int y = 0; y < 11; y++) {
+            Double lat2 = (double) y * 3 - y * 2;
+            Double long2 = (double) (y * 3 - y * 2) + 1;
+            // below is not my algorithm
+            double theta = long1 - long2;
+            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            // above is not my algorithm
+            distances.add(dist);
+        }
+        for (int z = 0; z < Integer.parseInt(input1); z ++){
+            Double temp = distances.get(0);
+            for (int a = 0; a < distances.size(); a ++){
+                if (temp > distances.get(a)){
+                    temp = distances.get(a);
+                }
+                if (a == distances.size() - 1){
+                    distances.remove(temp);
+                }
+            }
+            System.out.println(temp);
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
@@ -64,6 +107,6 @@ public class FinalProject {
         System.out.println(list);
         searchState(list);
         searchCity(list);
-        setCity(list);
+        findDistance(list);
     }
 }
